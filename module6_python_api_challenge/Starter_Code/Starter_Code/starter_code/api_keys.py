027a57132764597988291516e7939067b9509d1e
khayru <43963328+khayru@users.noreply.github.com>
2023-09-03 20:33:31 -0400

    Add files via upload

--- module6_python_api_challenge/Starter_Code/Starter_Code/starter_code/api_keys.py
+++ module6_python_api_challenge/Starter_Code/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "fdcbc9b0c0a84f73a9ca4d4febb6da2f"
+weather_api_key ="ed7ae7e580912eaca062da4c03e1b91e"
 
 # Geoapify API Key
-geoapify_key = "fdcbc9b0c0a84f73a9ca4d4febb6da2f"
+geoapify_key = "0bb6de5d6a55469b87f5a303ded9bd07"
